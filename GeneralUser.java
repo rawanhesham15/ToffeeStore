054701f2a7c6df95abd88dc8da9781e8fd73c098
__Rawan__ <rawanroni2003@gmail.com>
2023-05-06 02:42:40 +0300

    update GeneralUser class
    add new line after adding new user to the users.txt file

--- GeneralUser.java
+++ GeneralUser.java
@@ -18,19 +18,20 @@ class GneralUser {
         String email = sc.nextLine();
         System.out.print("Enter your Address: ");
         String address = sc.nextLine();
-        
+
         BufferedWriter out = null;
         try {
-            FileWriter fstream = new FileWriter("users.txt", true); //true tells to append data.
+            FileWriter fstream = new FileWriter("users.txt", true); // true tells to append data.
             out = new BufferedWriter(fstream);
             out.write("\n");
-            out.write(name +",");
-            out.write( pass + ",");
+            out.write(name + ",");
+            out.write(pass + ",");
             out.write(phoneNum + ",");
             out.write(email + ",");
             out.write(address + ",");
-            out.write("Status:Active "+",");
+            out.write("Status:Active " + ",");
             out.write("LoyaltyPoints: 0");
+            out.write("\n");
         }
 
         catch (IOException e) {
@@ -38,11 +39,11 @@ class GneralUser {
         }
 
         finally {
-            if(out != null) {
+            if (out != null) {
                 out.close();
             }
         }
-        String data[] = {name,pass, address, phoneNum, email};
+        String data[] = { name, pass, address, phoneNum, email };
         System.out.println("^^^^^Done You Are Now Registered.^^^^^");
         return data;
     }
@@ -50,41 +51,41 @@ class GneralUser {
     public void logIn() {
         String[][] userArray = new String[MAX_USERS][2];
         int numberOfUsers = 0;
-  
+
         // load the login data from the file
         File file = new File(FILE_PATH);
         try (Scanner scanner = new Scanner(file)) {
-           while (scanner.hasNextLine() && numberOfUsers < MAX_USERS) {
-              String line = scanner.nextLine();
-              String[] parts = line.split(",");
-              userArray[numberOfUsers][0] = parts[0]; // username
-              userArray[numberOfUsers][1] = parts[1]; // password
-              numberOfUsers++;
-           }
+            while (scanner.hasNextLine() && numberOfUsers < MAX_USERS) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                userArray[numberOfUsers][0] = parts[0]; // username
+                userArray[numberOfUsers][1] = parts[1]; // password
+                numberOfUsers++;
+            }
         } catch (FileNotFoundException e) {
-           System.out.println("File not found: " + FILE_PATH);
+            System.out.println("File not found: " + FILE_PATH);
         }
-  
+
         // prompt the user to enter their username and password
         Scanner input = new Scanner(System.in);
         System.out.print("Enter username: ");
         String username = input.nextLine();
         System.out.print("Enter password: ");
         String password = input.nextLine();
-  
+
         // search the userArray for a matching username and password
         int index = -1;
         for (int i = 0; i < numberOfUsers; i++) {
-           if (userArray[i][0].equals(username) && userArray[i][1].equals(password)) {
-              index = i;
-              break;
-           }
+            if (userArray[i][0].equals(username) && userArray[i][1].equals(password)) {
+                index = i;
+                break;
+            }
         }
-  
+
         if (index >= 0) {
-           System.out.println("Welcome " + userArray[index][0] + "!");
+            System.out.println("Welcome " + userArray[index][0] + "!");
         } else {
-           System.out.println("Invalid login.");
+            System.out.println("Invalid login.");
         }
     }
 
